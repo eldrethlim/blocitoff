ba6aa20682e04957eb7985c2dd0c5ff1d4e08631
JunXian <eldrethlim@gmail.com>
2014-07-31 11:14:27 +0000

    Refactored tests.

--- spec/features/task_management_spec.rb
+++ spec/features/task_management_spec.rb
@@ -6,29 +6,27 @@ describe 'Task management' do
       password: 'Password1', password_confirmation: 'Password1')
     @user.skip_confirmation!
     @user.save!
+    sign_in @user
   end
 
   it 'should allow a user to add a new task' do
-    sign_in @user, 'Password1'
-    fill_in 'Task name', with: 'This is a test task.'
+    fill_in_task
     click_button 'Add Task'
     expect(page).to have_content('New task added.')
-    expect(page).to have_content('This is a test task.')
+    expect(page).to have_content('A task')
     expect(current_path).to eq('/')
   end
 
   it 'should not allow a user to add a task without a name' do
-    sign_in @user, 'Password1'
     click_button 'Add Task'
     expect(page).to have_content('Error adding task.')
     expect(current_path).to eq('/')
   end
 
   it 'should allow a user to edit a task' do
-    sign_in @user, 'Password1'
-    fill_in 'Task name', with: 'This is another test task.'
+    fill_in_task
     click_button 'Add Task'
-    click_link 'This is another test task.'
+    click_link 'A task'
     fill_in 'Task name', with: 'This is the edited task.'
     click_button 'Edit Task'
     expect(page).to have_content('Task updated.')
@@ -37,10 +35,9 @@ describe 'Task management' do
   end
 
   it 'should not allow a user to edit a task into an empty name field' do
-    sign_in @user, 'Password1'
-    fill_in 'Task name', with: 'This is just another test task.'
+    fill_in_task
     click_button 'Add Task'
-    click_link 'This is just another test task.'
+    click_link 'A task'
     fill_in 'Task name', with: ""
     click_button 'Edit Task'
     expect(page).to have_content('Error updating task.')
@@ -48,8 +45,7 @@ describe 'Task management' do
   end
 
   it 'should allow a user to delete a task' do
-    sign_in @user, 'Password1'
-    fill_in 'Task name', with: 'A task'
+    fill_in_task
     click_button 'Add Task'
     click_link 'Delete'
     expect(page).to have_no_content('A task')
@@ -58,8 +54,7 @@ describe 'Task management' do
   end
 
   it 'should allow a user to complete a task' do
-    sign_in @user, 'Password1'
-    fill_in 'Task name', with: 'A task'
+    fill_in_task
     click_button 'Add Task'
     click_link 'Completed?'
     expect(page).to have_content('Task completed. Good job!')
@@ -67,18 +62,21 @@ describe 'Task management' do
   end
 
   it 'should allow a user to incomplete a task' do
-    sign_in @user, 'Password1'
-    fill_in 'Task name', with: 'a task'
+    fill_in_task
     click_button 'Add Task'
-    click_link 'Complete'
+    click_link 'Completed?'
     click_link 'Incomplete?'
     expect(page).to have_content("It's not done after all!")
     expect(current_path).to eq('/')
   end
 
   private
+  
+  def fill_in_task
+    fill_in 'Task name', with: 'A task'
+  end
 
-  def sign_in(user, password)
+  def sign_in(user)
     visit '/'
     fill_in 'Email', with: user.email
     fill_in 'Password', with: 'Password1'
