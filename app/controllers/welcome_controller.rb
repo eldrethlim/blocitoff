c2f1a4b6d22d931893ffc0e209d09d8a38f695de
JunXian <eldrethlim@gmail.com>
2014-08-01 10:01:54 +0000

    modified ajax add task

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     if current_user
-      @newtask = Task.new
-      @tasks = current_user.tasks.all
+      @tasks = current_user.tasks.all.order('tasks.created_at DESC')
     end
   end
 end
