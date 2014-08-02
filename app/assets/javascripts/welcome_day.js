656655bc97f1c4987a328436edd26c68eb1ab07a
JunXian <eldrethlim@gmail.com>
2014-08-02 14:21:39 +0000

    fix javascript problem, update ajax to display errors

--- app/assets/javascripts/welcome_day.js
+++ app/assets/javascripts/welcome_day.js
@@ -1,15 +1,18 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
+$(document).ready(function() {
+  var today = new Date();
+  var hourNow = today.getHours();
+  var greeting;
 
-if (hourNow > 18) {
-  greeting = 'Good evening';
-} else if (hourNow > 12) {
-  greeting = 'Good afternoon';
-} else if (hourNow > 0) {
-  greeting = 'Good morning';
-} else {
-  greeting = 'Welcome';
-}
+  if (hourNow > 18) {
+    greeting = 'Good evening';
+  } else if (hourNow > 12) {
+    greeting = 'Good afternoon';
+  } else if (hourNow > 0) {
+    greeting = 'Good morning';
+  } else {
+    greeting = 'Welcome';
+  }
 
-document.getElementById("welcome_user_container").innerHTML='<h3>' + greeting + '</h3>';
+  document.getElementById("welcome_user_script").innerHTML='<h3>' + greeting + ',</h3>';
+
+})
