7abc24addbf87e387fa2d6ebb24b425429ebb266
JunXian <eldrethlim@gmail.com>
2014-08-05 12:21:50 +0000

    removed old flash message codes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  after_filter :flash_headers
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def flash_headers
-    
-    return unless request.xhr?
-
-    response.headers['x-flash'] = flash[:error] unless flash[:error].blank?
-    response.headers['x-flash'] = flash[:notice] unless flash[:notice].blank?
-    repsonse.headers['x-flash'] = flash[:warning] unless flash[:warning].blank?
-
-    flash.discard
-  end
  
   protected
  
