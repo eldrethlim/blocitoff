77b94d9818ca60a9601c7b5cd3456992cd08514d
JunXian <eldrethlim@gmail.com>
2014-08-07 18:42:52 +0000

    friendly id fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  def should_generate_new_friendly_id?
+    new_record?
+  end
 end
